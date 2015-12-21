9864e51c19fd160dc01f492b3fc3ae7e5963a77f
PlayerNguyen <daudaua3782@gmail.com>
2015-12-21 09:16:23 +0700

    Update RandomClass.cs
    Add RandomChar().

--- RandomClass.cs
+++ RandomClass.cs
@@ -2,13 +2,14 @@ using System;
 using System.Linq;
 
 /// <summary>
-/// Random By Player Nguyen. Info https://github.com/PlayerNguyen/RandomAnyLanguage
+/// RandomAnyLanguage
+/// RandomAnyLanguage By Player Nguyen. Info https://github.com/PlayerNguyen/RandomAnyLanguage
 /// </summary>
 public class RandomClass
 {
 
     /// <summary>
-    /// Random Integer 
+    /// Random Integer Random C#
     /// </summary>
     /// <param name="min">Max value of random</param>
     /// <param name="max">Min value of random</param>
@@ -21,14 +22,13 @@ public class RandomClass
 
 
     /// <summary>
-    /// Random String
+    /// Random String Random C#
     /// </summary>
     /// <param name="chars">Aphabet Char. Use any type</param>
     /// <param name="length">Length of this String</param>
     /// <returns></returns>
     public static string RandomString(String chars, int length)
-    {
-        
+    {  
         var random = new Random();
         return new string(Enumerable.Repeat(chars, length)
           .Select(s => s[random.Next(s.Length)]).ToArray());
@@ -43,8 +43,19 @@ public class RandomClass
     /// <returns>double</returns>
     public static double RandomDouble(double min, double max)
     {
-        
         return new Random().NextDouble() * (max - min) + min; 
+    }
+
+    /// <summary>
+    /// Random Char Random C#
+    /// </summary>
+    /// <returns></returns>
+    public static char RandomChar()
+    {
+        string chars = "$%#@!*abcdefghijklmnopqrstuvwxyz1234567890?;:ABCDEFGHIJKLMNOPQRSTUVWXYZ^&";
+        Random rand = new Random();
+        int num = rand.Next(0, chars.Length - 1);
+        return chars[num];
     }
 
 }
